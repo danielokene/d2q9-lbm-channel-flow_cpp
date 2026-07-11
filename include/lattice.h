6732c6128e7ae889a6b428aa718dc6ce3312296d
Daniel Okene <okenedaniel44@gmail.com>
2026-07-11 13:52:34 +0100

    implement equillibrium initialization

--- include/lattice.h
+++ include/lattice.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <array>
+#include <vector>
+#include "parameters.h"
+#include "utilities.h"
 
 // defining the discrete lattice namespace
 namespace LBM
 {
     constexpr int Q = 9; // number of directions
 
-    using cell = std::array<double, Q>; // stores probability function of each lattice nodes
+    using Cell = std::array<double, Q>; // stores probability function of each lattice nodes
 
     constexpr std::array<int, Q> cx =
     {
@@ -36,4 +39,27 @@ namespace LBM
         1.0/36.0,
         1.0/36.0
     };
+
+    // initialize the entire lattice
+    void initialize(
+        std::vector<Cell>& f,
+        const Parameters& params)
+    {
+        const int totalCells = params.nx * params.ny;
+
+        f.resize(totalCells);
+
+        for (int y = 0; y < params.ny; ++y)
+        {
+            for (int x = 0; x < params.nx; ++x)
+            {
+                const int n = index(x, y, params.nx);
+
+                for (int k = 0; k < Q; ++k)
+                {
+                    f[n][k] = equilibrium(k, 1.0, 0.0, 0.0);
+                }
+            }
+        }
+    }
 }
